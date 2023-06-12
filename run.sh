c2447dec4808a3d111127b63e2f8145e4478a411
Emilio Graff <1@emil.io>
2023-06-12 08:29:32 -0700

    Use `screen`

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Check requirements.
+if ! command -v virtualenv >/dev/null 2>&1; then
+    echo "Please install `virtualenv`."
+    exit 1
+fi
+
+if ! command -v screen >/dev/null 2>&1; then
+    echo "Please install `screen`."
+    exit 2
+fi
 
 # Requires Python 3.8+, `pip`, and `virtualenv`
 if [ -d venv ]; then
@@ -7,8 +17,11 @@ else
     echo "Creating virtual environment."
     virtualenv venv
 fi
+
 PYTHON=venv/bin/python
 $PYTHON -m pip install -r requirements.txt
 
-echo "Running program..."
-$PYTHON exec.py
+echo "Running data collection on screen `speedtest-logger-collect`..."
+screen -dmS speedtest-logger-collect $PYTHON exec.py
+
+exit 0
