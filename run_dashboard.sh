ee564c226f68842cd4b4d4fb876628be80cdfc6d
Emilio Graff <1@emil.io>
2023-06-13 21:30:43 -0700

    Higher port

--- run_dashboard.sh
+++ run_dashboard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Running dashboard on screen ``speedtest-logger-dashboard``..."
-screen -dmS speedtest-logger-dashboard venv/bin/flask --app dashboard.flask_app run --host=0.0.0.0 --port 1000
+screen -dmS speedtest-logger-dashboard venv/bin/flask --app dashboard.flask_app run --host=0.0.0.0 --port 10000
 
 exit 0
