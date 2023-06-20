fffbc0b75130836a12ef9448fdfb90afbab54c73
Emilio Graff <1@emil.io>
2023-06-20 15:20:30 -0700

    Args are strings

--- dashboard/flask_app.py
+++ dashboard/flask_app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', defaults={'n_records': config.n_records})
 @app.route('/<n_records>')
-def main(n_records: int):
-    sources = proc_results(n_records)
+def main(n_records: str):
+    sources = proc_results(int(n_records))
     smoothed = smooth(sources)
     return down_up(smoothed)
