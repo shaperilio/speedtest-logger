52ed9e4cf947039cdb7e9b6f0a69f9860554cb83
Emilio Graff <1@emil.io>
2023-06-20 05:56:05 -0700

    Show less points by default

--- config.py
+++ config.py
@@ -28,5 +28,5 @@ displaying results.
 n_attempts: int = 5
 """Number of times to execute `speedtest` while the return code is not 0. Must be at least 1."""
 
-n_records = 500
+n_records = 300
 """Maximum number of records to show on the plot."""
