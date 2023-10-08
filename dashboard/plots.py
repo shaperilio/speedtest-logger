b19a2c0ba1b2f5688ac2f8cc4b82e26458135319
Emilio Graff <1@emil.io>
2023-10-08 11:37:02 -0700

    Also count tests

--- dashboard/plots.py
+++ dashboard/plots.py
@@ -214,10 +214,13 @@ def stats_by_val(by_val: Dict[str, ColumnDataSource], val_name: str) -> Dict[str
             by_hr[hour].append((dn, up))
         by_hr = dict(sorted(by_hr.items(), key=lambda i: i[0]))
 
-        mean_dn_by_hr = [numpy.mean([i[0] for i in by_hr[k]]) for k in by_hr.keys()]
-        mean_up_by_hr = [numpy.mean([i[1] for i in by_hr[k]]) for k in by_hr.keys()]
-        std_dn_by_hr = [numpy.std([i[0] for i in by_hr[k]]) for k in by_hr.keys()]
-        std_up_by_hr = [numpy.std([i[1] for i in by_hr[k]]) for k in by_hr.keys()]
+        dn_by_hr = [[i[0] for i in by_hr[k]] for k in by_hr.keys()]
+        up_by_hr = [[i[1] for i in by_hr[k]] for k in by_hr.keys()]
+        mean_dn_by_hr = [numpy.mean(d) for d in dn_by_hr]
+        mean_up_by_hr = [numpy.mean(u) for u in up_by_hr]
+        std_dn_by_hr = [numpy.std(d) for d in dn_by_hr]
+        std_up_by_hr = [numpy.std(u) for u in up_by_hr]
+        n_by_hr = [len(d) for d in dn_by_hr]
 
         by_val_speeds[nickname] = {}
         by_val_speeds[nickname][val_name] = list(by_hr.keys())
@@ -225,6 +228,7 @@ def stats_by_val(by_val: Dict[str, ColumnDataSource], val_name: str) -> Dict[str
         by_val_speeds[nickname]['upload_mbps_mean'] = mean_up_by_hr
         by_val_speeds[nickname]['download_mbps_std'] = std_dn_by_hr
         by_val_speeds[nickname]['upload_mbps_std'] = std_up_by_hr
+        by_val_speeds[nickname]['num_tests'] = n_by_hr
 
     by_val: Dict[str, ColumnDataSource] = {}
     for nickname in sorted(by_val_speeds.keys()):
@@ -235,6 +239,7 @@ def stats_by_val(by_val: Dict[str, ColumnDataSource], val_name: str) -> Dict[str
                 'upload_mbps_mean': by_val_speeds[nickname]['upload_mbps_mean'],
                 'download_mbps_std': by_val_speeds[nickname]['download_mbps_std'],
                 'upload_mbps_std': by_val_speeds[nickname]['upload_mbps_std'],
+                'num_tests': by_val_speeds[nickname]['num_tests'],
                 }
         by_val[nickname] = ColumnDataSource(data)
 
@@ -263,7 +268,15 @@ def down_up_by_hour(sources: Dict[str, ColumnDataSource]) -> str:
         tooltips=[
             ('Interface', '@nickname'),
             ('Avg down', '@download_mbps_mean{0.0} +/- @download_mbps_std{0.0} Mbps'),
-            ('Avg up rate', ' @upload_mbps_mean{0.0} +/- @upload_mbps_std{0.0} Mbps')
+            ('Avg up rate', ' @upload_mbps_mean{0.0} +/- @upload_mbps_std{0.0} Mbps'),
+            ('Num tests', ' @num_tests{,}')
+        ],
+        mode='mouse',
+        renderers=dots,
+    )
+    fig.add_tools(hover)
+
+    return file_html(fig, CDN, 'Speedtest log')
         ],
         mode='mouse',
         renderers=dots,
