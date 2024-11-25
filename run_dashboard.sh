b54e0eabd47418e8a205d1890426198c63a9aa3a
Emilio Graff <1@emil.io>
2024-11-25 09:33:04 -0800

    Do not use reload; data loader thread will run at least twice.

--- run_dashboard.sh
+++ run_dashboard.sh
@@ -6,6 +6,6 @@ if [ ! -f speedtest-logger-marker ]; then
 fi
 
 echo "Running dashboard on screen ``speedtest-logger-dashboard``..."
-screen -dmS speedtest-logger-dashboard venv/bin/flask --app dashboard.flask_app run --host=0.0.0.0 --port 10000 --reload
+screen -dmS speedtest-logger-dashboard venv/bin/flask --app dashboard.flask_app run --host=0.0.0.0 --port 10000
 
 exit 0
